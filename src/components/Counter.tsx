2ffdb225942a6e4326ec6955539b095a4b315c9d
Tomo-kw <mulberryfield412@yahoo.ne.jp>
2022-08-14 17:28:48 +0900

    Counterコンポーネント作成

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,23 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
+
+// reducerが受け取るactionの型を定義
+type Action = 'DECREMENT' | 'INCREMENT' | 'DOUBLE' | 'RESET'
+
+// 現在の状態とactionにもとづいて次の状態を返す
+const reducer = (currentCount: number, action: Action) => {
+    switch (action) {
+        case 'INCREMENT':
+            return currentCount + 1
+        case 'DECREMENT':
+            return currentCount - 1
+        case 'DOUBLE':
+            return currentCount * 2
+        case 'RESET':
+            return 0
+        default:
+            return currentCount    
+    }
+}
 
 type CounterProps = {
     initialValue: number
@@ -6,16 +25,23 @@ type CounterProps = {
 
 const Counter = (props: CounterProps) => {
     const { initialValue } = props
+    const [count, dispatch] = useReducer(reducer, initialValue)
     // カウントが保持する1つの状態をuseState()で宣言。引数に初期値を指定
     // countが現在の状態、setCountカウントが状態を更新する関数
-    const [count, setCount] = useState(initialValue)
+    // const [count, setCount] = useState(initialValue)
 
     return (
         <div>
             <p>Count: {count}</p>
+            {/* dispatch関数にactionを渡して、状態を更新する */}
+            <button onClick={() => dispatch('DECREMENT')}>-</button>
+            <button onClick={() => dispatch('INCREMENT')}>+</button>
+            <button onClick={() => dispatch('DOUBLE')}>×2</button>
+            <button onClick={() => dispatch('RESET')}>Reset</button>
+
             {/* setCountを呼ぶことで状態を更新 */}
-            <button onClick={() => setCount(count - 1)}>-</button>
-            <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
+            {/* <button onClick={() => setCount(count - 1)}>-</button>
+            <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button> */}
         </div>
     )
 }
