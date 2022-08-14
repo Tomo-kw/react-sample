f848a02560d10c4e1f90edad1aed4583038ca86a
Tomo-kw <mulberryfield412@yahoo.ne.jp>
2022-08-14 10:38:27 +0900

    ContainerSampleコンポーネント作成

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Hello from './components/Hello';
+// import Hello from './components/Hello';
+import ContainerSample from './components/ContainerSample';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Hello />
+    <ContainerSample />
   </React.StrictMode>
 );
 
